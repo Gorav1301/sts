45cce210604cd8c3c2210010c0427bd51982fc47
Gorav1301 <goravjhabakh1301@gmail.com>
2025-02-03 21:02:01 +0530

    Middle Element

---
+++ src/MiddleElement.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class MiddleElement {
+    static Node head = null;
+
+    public static void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    static void midElement() {
+        if (head == null) {
+            System.out.println("Failed to read middle element");
+            return;
+        }
+        Node slow = head;
+        Node fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        System.out.println(slow.data);
+    }
+    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int data = sc.nextInt();
+            insert(data);
+        }
+        midElement();
+    }
+}
